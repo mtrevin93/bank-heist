8a7ee07487d433041ec597691b1f9053d5fb28a2
Michael Trevino <mtrevin93@gmail.com>
2021-08-27 15:20:23 -0400

    initial commit

---
+++ bank-heist/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bank_heist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
