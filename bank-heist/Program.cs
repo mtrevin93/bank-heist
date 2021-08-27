3fe7a4c01d21541de2735aec6b30fb6b919af93f
Michael Trevino <mtrevin93@gmail.com>
2021-08-27 16:28:39 -0400

    member adder now loops until empty string entered as name

--- bank-heist/Program.cs
+++ bank-heist/Program.cs
@@ -8,15 +8,22 @@ namespace Heist
         {
             Console.WriteLine("Plan Your Heist!");
             //Create first team member
-            Member starterMember = new Member();
-            Console.WriteLine("Enter Team member's name: ");
-            starterMember.Name = Console.ReadLine();
-            Console.WriteLine("Enter Team member's skill level");
-            starterMember.SkillLevel = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Enter team member's courage factor");
-            starterMember.CourageFactor = Double.Parse(Console.ReadLine());
-            //Display team member
-            Console.WriteLine($"Heisting with {starterMember.Name}\nSkill Level: {starterMember.SkillLevel}\nCourage Factor: {starterMember.CourageFactor}");
+            while(true){
+            string newMemberName = Console.ReadLine();
+            if(newMemberName==""){
+                break;
+            }
+            else{
+                Member newMember = new Member();
+                Console.WriteLine("Enter Team member's name: ");
+                Console.WriteLine("Enter Team member's skill level");
+                newMember.SkillLevel = UInt32.Parse(Console.ReadLine());
+                Console.WriteLine("Enter team member's courage factor");
+                newMember.CourageFactor = Double.Parse(Console.ReadLine());
+                //Display team member
+                Console.WriteLine($"Heisting with {newMember.Name}\nSkill Level: {newMember.SkillLevel}\nCourage Factor: {newMember.CourageFactor}");
+            }
+            }
         }
     }
 }
---
+++ bank-heist/Team.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+namespace Heist
+{
+public class Team {
+public List<Member> MemberList { get; set; }
+
+}
+}
