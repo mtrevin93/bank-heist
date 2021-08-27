a8835a1d6ddfbd9f9b6f306c2e78377a3276fc95
Michael Trevino <mtrevin93@gmail.com>
2021-08-27 16:50:05 -0400

    now allows input for multiple team members & displays tem

--- bank-heist/Program.cs
+++ bank-heist/Program.cs
@@ -7,22 +7,37 @@ namespace Heist
         static void Main(string[] args)
         {
             Console.WriteLine("Plan Your Heist!");
+            Team HeistTeam = new Team();
             //Create first team member
-            while(true){
+            do
+            {
+            Member newMember = new Member();
+            Console.WriteLine("Enter Team member's name: ");
             string newMemberName = Console.ReadLine();
+            //Exit loop if newMemberName is empty string
             if(newMemberName==""){
                 break;
             }
             else{
-                Member newMember = new Member();
-                Console.WriteLine("Enter Team member's name: ");
+                //Create new member
+                newMember.Name = newMemberName;
                 Console.WriteLine("Enter Team member's skill level");
                 newMember.SkillLevel = UInt32.Parse(Console.ReadLine());
                 Console.WriteLine("Enter team member's courage factor");
                 newMember.CourageFactor = Double.Parse(Console.ReadLine());
+                HeistTeam.MemberList.Add(newMember);
                 //Display team member
-                Console.WriteLine($"Heisting with {newMember.Name}\nSkill Level: {newMember.SkillLevel}\nCourage Factor: {newMember.CourageFactor}");
-            }
+            }} while(true);
+
+            //Display team details
+            Console.WriteLine($"Heisting with {HeistTeam.MemberList.Count} members");
+            Console.WriteLine("----------------THE TEAM----------------");
+            foreach(Member member in HeistTeam.MemberList)
+            {
+                Console.WriteLine($"{member.Name}");
+                Console.WriteLine($"Skill Level: {member.SkillLevel}");
+                Console.WriteLine($"Courage Factor: {member.CourageFactor}");
+                Console.WriteLine("--------------------------------");
             }
         }
     }
--- bank-heist/Team.cs
+++ bank-heist/Team.cs
@@ -3,6 +3,9 @@ namespace Heist
 {
 public class Team {
 public List<Member> MemberList { get; set; }
-
+public Team()
+{
+    MemberList = new List<Member>();
+}
 }
 }
