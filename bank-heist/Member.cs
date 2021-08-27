fdd794b46ae59f6665ca4038003b07606169840a
Michael Trevino <mtrevin93@gmail.com>
2021-08-27 15:58:03 -0400

    phase1 complete

--- bank-heist/Member.cs
+++ bank-heist/Member.cs
@@ -5,7 +5,9 @@ public class Member
 {
 public string Name {get; set;}
 public uint SkillLevel {get; set;}
-[Range(0, 2.0)]public double CourageFactor { get; set; }
+public double CourageFactor { get; set; }
 public Member(){}
+
 }
 }
+
